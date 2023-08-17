bc1769c1efa7e241883c070c4362362cba3ab359
Bigdreamer17 <estifanosfm@gmail.com>
2023-08-17 14:43:09 +0300

    Basics of EF Core

---
+++ Day7/Blog/Models/Post.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class Post : BaseEntity
+    {
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+        }
+        
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public virtual ICollection<Comment> Comments {get; set;}
+    }
+}
